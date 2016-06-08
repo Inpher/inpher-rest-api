99543e72f02322b30148f50f9dafb3c75d9e73fb
blagovesta <blagovesta@inpher.io>
2016-06-08 15:57:59 +0200

    added sfs methods

--- src/main/java/rest/service/MyJerseyPage.java
+++ src/main/java/rest/service/MyJerseyPage.java
@@ -1,6 +1,9 @@
 package rest.service;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.Security;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -11,13 +14,15 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Cookie;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
+import org.apache.commons.io.FileUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.inpher.clientapi.FrontendPath;
 import org.inpher.clientapi.InpherClient;
 import org.inpher.clientapi.InpherUser;
@@ -25,26 +30,28 @@ import org.inpher.clientapi.efs.BackendIterator;
 import org.inpher.clientapi.efs.Element;
 import org.inpher.clientapi.efs.SearchableFileSystem;
 import org.inpher.clientapi.efs.exceptions.ParentNotFoundException;
+import org.inpher.clientapi.efs.exceptions.PathIsDirectoryException;
 import org.inpher.clientapi.efs.exceptions.PathNotFoundException;
 import org.inpher.clientapi.exceptions.AuthenticationException;
+import org.inpher.clientapi.exceptions.ExistingMemberException;
 import org.inpher.clientapi.exceptions.ExistingUserException;
 import org.inpher.clientapi.exceptions.InpherException;
 import org.inpher.clientapi.exceptions.InpherRuntimeException;
-import org.json.JSONObject;
+import org.inpher.clientapi.exceptions.NonEmptyDirectoryException;
+import org.inpher.clientapi.exceptions.PathNotOwnedByUserException;
 import org.json.simple.JSONArray;
+
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
 
 @Path("/")
 public class MyJerseyPage {
 	private static InpherClient inpherClient;
 	private static Map<String, SearchableFileSystem> sfss;
 	private static String AUTH_TOKEN = "auth_token";
-	
-	@GET
-	@Produces(MediaType.TEXT_HTML)
-	public String sayHtmlHello() {
-		return "Hello from Jersey";
-	}
+
 	static {
+		Security.addProvider(new BouncyCastleProvider());
 		sfss = new ConcurrentHashMap<String, SearchableFileSystem>();
 		try {
 			inpherClient = InpherClient.getClient("D:\\workspace\\rest.service\\src\\config.properties");
@@ -52,94 +59,243 @@ public class MyJerseyPage {
 			e.printStackTrace();
 		}
 	}
-	
-	@Path("hi")
-	@POST
+
+	@GET
 	@Produces(MediaType.TEXT_HTML)
-	public String hi() {
+	public String sayHtmlHello() {
 		return "Hello from Jersey";
 	}
-	
+
 	@Path("register")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response register(User user){
+	public Response register(User user) {
 		if (user.getName() == null || user.getPassword() == null) {
-            return Response.status(400).entity("password and name should not be empty").build();
-        }
-        try {
-            inpherClient.registerUser(
-                    new InpherUser(user.getName(), user.getPassword()));
-        } catch (ExistingUserException e) {
-            return Response.status(409).entity("user already exists").build();
-        }
-        return Response.status(200).entity("success").build();
+			return Response.status(400).entity("password and name should not be empty").build();
+		}
+		try {
+			inpherClient.registerUser(new InpherUser(user.getName(), user.getPassword()));
+		} catch (ExistingUserException e) {
+			return Response.status(409).entity("user already exists").build();
+		}
+		return Response.ok().build();
 	}
-	
+
 	@Path("login")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response login(User user){
+	public Response login(User user) {
 		SearchableFileSystem sfs;
-        try {
-            sfs = inpherClient.loginUser(new InpherUser(user.getName(), user.getPassword()));
-        } catch (AuthenticationException e) {
-            return Response.status(409).entity("Authentication failed").build();
-        }
-		
+		try {
+			sfs = inpherClient.loginUser(new InpherUser(user.getName(), user.getPassword()));
+		} catch (AuthenticationException e) {
+			return Response.status(409).entity("Authentication failed").build();
+		}
+
 		String result = "Person logged in successfully : " + user.getName();
-	    String token = UUID.randomUUID().toString();
+		String token = UUID.randomUUID().toString();
 		NewCookie cookie = new NewCookie(AUTH_TOKEN, token);
 		sfss.put(token, sfs);
 		return Response.status(201).entity(result).cookie(cookie).build();
 	}
-	
+
 	@Path("mkdir")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response mkdir(String dir, @CookieParam("auth_token") Cookie cookie){
+	public Response mkdir(String dir, @CookieParam("auth_token") Cookie cookie) {
 		SearchableFileSystem sfs = sfss.get(cookie.getValue());
-        if(sfs == null){
-            return Response.status(409).entity("Authentication failed").build();
-        }
+		if (sfs == null) {
+			return Response.status(409).entity("Authentication failed").build();
+		}
 		try {
 			sfs.mkdir(FrontendPath.parse(dir));
 		} catch (ParentNotFoundException e) {
-            return Response.status(400).entity("The parent of the dir does not exists.").build();
+			return Response.status(400).entity("The parent of the dir does not exist.").build();
 		} catch (InpherRuntimeException e) {
-            return Response.status(400).entity("An error occured. Please check: " + e.getMessage())
-            		.build();
+			return Response.status(400).entity("An error occured. Please check: " + e.getMessage()).build();
 		}
 
 		String result = "Dir created successfully : " + dir;
 		return Response.status(201).entity(result).build();
 	}
-	
+
 	@Path("listDir")
 	@POST
-	@Consumes(MediaType.APPLICATION_JSON)	
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response listDir(String dir, @CookieParam("auth_token") Cookie cookie){
+	public Response listDir(String dir, @CookieParam("auth_token") Cookie cookie) {
 		SearchableFileSystem sfs = sfss.get(cookie.getValue());
-        if(sfs == null){
-            return Response.status(409).entity("Authentication failed").build();
-        }
-        JSONArray arr = new JSONArray();
+		if (sfs == null) {
+			return Response.status(409).entity("Authentication failed").build();
+		}
+		JSONArray arr = new JSONArray();
 		try {
 			BackendIterator<Element> iterator = sfs.list(FrontendPath.parse(dir));
-			while(iterator.hasNext()){
+			while (iterator.hasNext()) {
 				Element el = iterator.next();
 				arr.add(el);
 			}
 		} catch (InpherRuntimeException e) {
-            return Response.status(400).entity("An error occured. Please check: " + e.getMessage())
-            		.build();
+			return Response.status(400).entity("An error occured. Please check: " + e.getMessage()).build();
 		} catch (PathNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			return Response.status(400).entity("The dir does not exist.").build();
 		}
 		return Response.ok(arr).build();
 	}
+
+	@Path("upload")
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(@FormDataParam("content") final InputStream content,
+			@FormDataParam("content") FormDataContentDisposition contentDispositionHeader,
+			@FormDataParam("name") String name, @CookieParam("auth_token") Cookie cookie) {
+		SearchableFileSystem sfs = sfss.get(cookie.getValue());
+
+		if (sfs == null) {
+			return Response.status(409).entity("Authentication failed").build();
+		}
+		File file = new File("temp.tmp");
+		try {
+			FileUtils.copyInputStreamToFile(content, file);
+		} catch (IOException e) {
+			return Response.status(400).entity("An error occured. Please check: " + e.getMessage()).build();
+		}
+		try {
+			sfs.upload(file, FrontendPath.parse(name));
+		} catch (ParentNotFoundException e) {
+			return Response.status(400).entity("The parent of the dir does not exists.").build();
+		} catch (InpherRuntimeException e) {
+			return Response.status(400).entity("An error occured. Please check: " + e.getMessage()).build();
+		}
+		return Response.ok("file uploaded").build();
+	}
+
+	@Path("download")
+	@GET
+	@Produces("text/plain")
+	public Response downloadFile(@QueryParam("fileName") String fileName, @CookieParam("auth_token") Cookie cookie) {
+		if (cookie == null) {
+			return Response.status(409).entity("Authentication failed").build();
+		}
+		SearchableFileSystem sfs = sfss.get(cookie.getValue());
+
+		if (sfs == null) {
+			return Response.status(409).entity("Authentication failed").build();
+		}
+		File file = new File("temp.tmp");
+		FrontendPath filePath = FrontendPath.parse(fileName);
+		try {
+			sfs.download(filePath, file);
+		} catch (PathNotFoundException e) {
+			return Response.status(400).entity("The file does not exist.").build();
+		} catch (PathIsDirectoryException e) {
+			return Response.status(400).entity("The path points to a directory").build();
+		} catch (InpherRuntimeException e) {
+			return Response.status(400).entity("An error occured. Please check: " + e.getMessage()).build();
+		}
+		ResponseBuilder response = Response.ok((Object) file);
+		response.header("Content-Disposition", "attachment; filename=\"" + filePath.getLastElementName() + "\"");
+		return response.build();
+	}
+
+	@Path("delete")
+	@GET
+	public Response delete(@QueryParam("fileName") String fileName, @QueryParam("recursive") boolean recursive,
+			@CookieParam("auth_token") Cookie cookie) {
+		if (cookie == null) {
+			return Response.status(409).entity("Authentication failed").build();
+		}
+		SearchableFileSystem sfs = sfss.get(cookie.getValue());
+
+		if (sfs == null) {
+			return Response.status(409).entity("Authentication failed").build();
+		}
+		try {
+			sfs.delete(FrontendPath.parse(fileName), recursive);
+		} catch (NonEmptyDirectoryException e) {
+			return Response.status(400).entity("The path points to a directory that is not emptry").build();
+		} catch (PathNotOwnedByUserException e) {
+			return Response.status(400).entity("You cannot delete the resource. The path does not belong to you")
+					.build();
+		} catch (PathNotFoundException e) {
+			return Response.status(400).entity("The resource does not exist.").build();
+		} catch (InpherRuntimeException e) {
+			return Response.status(400).entity("An error occured. Please check: " + e.getMessage()).build();
+		}
+		return Response.ok("resource deleted").build();
+	}
+
+	@Path("exists")
+	@GET
+	public Response exists(@QueryParam("fileName") String fileName, @CookieParam("auth_token") Cookie cookie) {
+		if (cookie == null) {
+			return Response.status(409).entity("Authentication failed").build();
+		}
+		SearchableFileSystem sfs = sfss.get(cookie.getValue());
+
+		if (sfs == null) {
+			return Response.status(409).entity("Authentication failed").build();
+		}
+		boolean exists = sfs.exists(FrontendPath.parse(fileName));
+		return Response.ok(exists).build();
+	}
+
+	@Path("isMember")
+	@GET
+	public Response isMember(@QueryParam("groupName") String groupName, @QueryParam("userName") String userName,
+			@CookieParam("auth_token") Cookie cookie) {
+		if (cookie == null) {
+			return Response.status(409).entity("Authentication failed").build();
+		}
+		SearchableFileSystem sfs = sfss.get(cookie.getValue());
+
+		if (sfs == null) {
+			return Response.status(409).entity("Authentication failed").build();
+		}
+		boolean isMember = sfs.isMember(groupName, userName);
+		return Response.ok(isMember).build();
+	}
+
+	@Path("owner")
+	@GET
+	public Response owner(@QueryParam("fileName") String fileName, @CookieParam("auth_token") Cookie cookie) {
+		if (cookie == null) {
+			return Response.status(409).entity("Authentication failed").build();
+		}
+		SearchableFileSystem sfs = sfss.get(cookie.getValue());
+
+		if (sfs == null) {
+			return Response.status(409).entity("Authentication failed").build();
+		}
+		String owner;
+		try {
+			owner = sfs.elementOwner(FrontendPath.parse(fileName));
+		} catch (PathNotFoundException e) {
+			return Response.status(400).entity("The resource does not exist.").build();
+		} catch (InpherRuntimeException e) {
+			return Response.status(400).entity("An error occured. Please check: " + e.getMessage()).build();
+		}
+		return Response.ok(owner).build();
+	}
+
+	@Path("addUser")
+	@GET
+	public Response addUser(@QueryParam("groupName") String groupName, @QueryParam("userName") String userName,
+			@CookieParam("auth_token") Cookie cookie) {
+		if (cookie == null) {
+			return Response.status(409).entity("Authentication failed").build();
+		}
+		SearchableFileSystem sfs = sfss.get(cookie.getValue());
+
+		if (sfs == null) {
+			return Response.status(409).entity("Authentication failed").build();
+		}
+		try {
+			sfs.addUser(groupName, userName);
+		} catch (ExistingMemberException e) {
+			return Response.status(400).entity("The user is already a part of the group.").build();
+		}
+		return Response.ok("user added").build();
+	}
+
 }
-
-
