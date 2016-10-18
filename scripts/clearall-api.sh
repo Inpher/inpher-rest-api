6e2610eba10d26c1942dfb17cf48aff0dfcc1698
Inpher on api.inpher.io <inpher@api.inpher.io>
2016-10-18 12:32:57 +0000

    devops branch (for demo environment only)

--- scripts/clearall-api.sh
+++ scripts/clearall-api.sh
@@ -6,14 +6,15 @@ OUT="$(echo "$OUT"; echo "<br>Stoppig zookeeper: <br>"; docker stop zookeeper)"
 OUT="$(echo "$OUT"; echo; docker rm -v zookeeper)"
 OUT="$(echo "$OUT"; echo "<br>Stopping rabbitMQ: <br>"; docker stop rabbitmq)"
 OUT="$(echo "$OUT"; echo; docker rm -v rabbitmq)"
+OUT="$(echo "$OUT"; echo "<br>Stopping HDFS: <br>"; docker stop hdfs)"
+OUT="$(echo "$OUT"; echo; docker rm -v hdfs)"
+OUT="$(echo "$OUT"; echo "<br>Restarting rabbitMQ: <br>"; docker run --name hdfs -d -t -p 9000:9000 -p 50070:50070 sequenceiq/hadoop-docker)"
 OUT="$(echo "$OUT"; echo "<br>Restarting rabbitMQ: <br>"; docker run --name rabbitmq -td -p 5672:5672 rabbitmq)"
 OUT="$(echo "$OUT"; echo "<br>Restarting elastic: <br>"; docker run --name elastic -td -p 9300:9300 -p 9200:9200 inpher/elastic-frequency:_ultra)"
 OUT="$(echo "$OUT"; echo "<br>Restarting zookeeper: <br>"; docker run -td --name zookeeper -p 2181:2181 jplock/zookeeper)" 
-OUT="$(echo "$OUT"; echo "<br>Restarting HDFS and HUE: <br>"; /home/ubuntu/projects/inpher-rest-api/scripts/install-hue)" 
 OUT="$(echo "$OUT"; echo "<br>Restarting Tomcat: <br>")"
+sleep 5
 /home/ubuntu/projects/inpher-rest-api/scripts/restart-tomcat 
-OUT="$(echo "$OUT"; echo "<br>Restarting Kibana: <br>"; /home/ubuntu/projects/inpher-rest-api/scripts/restart-kibana)"
-OUT="$(echo "$OUT"; echo "<br>Restarting Ultraweb: <br>"; /home/ubuntu/projects/ultraweb/scripts/restart-ultra)"
 
  echo "Content-type: text/html"
  echo ""
